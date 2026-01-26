724999079b69f8e975ca2dfed7bf9faed9896946
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-01-26 11:45:38 +0530

    adding database url and sonarcube config+jacaco config

---
+++ src/test/java/com/lnreddy/friendly_ecommerce/shared/util/IdGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.lnreddy.friendly_ecommerce.shared.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdGeneratorTest {
+
+    @Test
+    void generate_shouldReturnNonNullId() {
+//        String id = IdGenerator.generate();
+//        assertNotNull(id);
+    }
+}
